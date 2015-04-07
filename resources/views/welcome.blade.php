5253ffa82c79517a6cc7eaad0bb8807c3fb0a70a
Jim Baldwin <jim@hvaldwin.net>
2015-04-07 12:26:19 -0700

    add github repos to demo page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,8 +4,18 @@
 	<div class="container">
 		<h1>Jim Baldwin</h1>
 		<ul>
-			<li><a href="http://todo.jimbaldwin.net">TODO app in Laravel 5</a></li>
-			<li><a href="http://services.jimbaldwin.net">Simple REST API</a></li>
+			<li>
+				<a href="http://todo.jimbaldwin.net">TODO app in Laravel 5</a>
+				<ul>
+					<li><a href="https://github.com/jimbaldwin123/todo-app">GitHub repo</a></li>
+				</ul>
+			</li>
+			<li>
+				<a href="http://services.jimbaldwin.net">Simple REST API</a>
+				<ul>
+					<li><a href="https://github.com/jimbaldwin123/rest-api">GitHub repo</a></li>
+				</ul>
+			</li>
 			<li><a href="docs/Support_Site_Requirements.docx">Support Site Requirements</a></li>
 			<li><a href="docs/Support_Site_Screens.docx">Support Site Screens</a></li>
 			<li><a href="docs/geo.py.zip" download="geo.py.zip">Lat/Long Decimal Conversion in Python</a></li>
