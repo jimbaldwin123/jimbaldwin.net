ac20bd9beb75cfbc13fb0dc85be64b0a93358e2d
Jim Baldwin <jim@hvaldwin.net>
2015-04-07 11:15:58 -0700

    add todo and services.jimbaldwin.net to demo site

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,6 +4,8 @@
 	<div class="container">
 		<h1>Jim Baldwin</h1>
 		<ul>
+			<li><a href="http://todo.jimbaldwin.net">TODO app in Laravel 5</a></li>
+			<li><a href="http://services.jimbaldwin.net">Simple REST API</a></li>
 			<li><a href="docs/Support_Site_Requirements.docx">Support Site Requirements</a></li>
 			<li><a href="docs/Support_Site_Screens.docx">Support Site Screens</a></li>
 			<li><a href="docs/geo.py.zip" download="geo.py.zip">Lat/Long Decimal Conversion in Python</a></li>
