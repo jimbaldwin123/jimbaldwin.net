d46cf65661319fa6f08393968ead39c72fe21119
Jim Baldwin <jim@hvaldwin.net>
2015-04-08 07:45:30 -0700

    remove python lat/long app from list

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -18,7 +18,6 @@
 			</li>
 			<li><a href="docs/Support_Site_Requirements.docx">Support Site Requirements</a></li>
 			<li><a href="docs/Support_Site_Screens.docx">Support Site Screens</a></li>
-			<li><a href="docs/geo.py.zip" download="geo.py.zip">Lat/Long Decimal Conversion in Python</a></li>
 			<li><a href="/switchbuilder">jQuery/AJAX Demo - Screencast</a></li>
 		</ul>
 	</div>
