0a709987f299100554abcc398c4cfbcd006370bb
Jim Baldwin <jim@hvaldwin.net>
2015-04-08 08:14:30 -0700

    add JSON software screen shots

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,17 +5,26 @@
 		<h1>Jim Baldwin</h1>
 		<ul>
 			<li>
-				<a href="http://todo.jimbaldwin.net">TODO app in Laravel 5</a>
+				<a href="http://todo.jimbaldwin.net">TODO app in Laravel 5</a> &ndash; In progress, see todo list in app for future enhancements.
 				<ul>
 					<li><a href="https://github.com/jimbaldwin123/todo-app">GitHub repo</a></li>
 				</ul>
 			</li>
 			<li>
-				<a href="http://services.jimbaldwin.net">Simple REST API</a>
+				<a href="http://services.jimbaldwin.net">Simple REST API without framework</a>
 				<ul>
 					<li><a href="https://github.com/jimbaldwin123/rest-api">GitHub repo</a></li>
 				</ul>
 			</li>
+			<li>JSON-driven JavaScript animation, plain JS w/o jQuery &ndash; Annotated software product screen shots<br>(legacy pages, nav layout and lightbox broken)</li>
+			<ul>
+				<li><a href="http://utlity2012.apcon.com/products/enterpoint.php">Animated Screen Shot 1</a></li>
+				<li><a href="http://utlity2012.apcon.com/products/webx.php">Animated Screen Shot 2</a></li>
+				<li><a href="http://utlity2012.apcon.com/products/netvis.php">Animated Screen Shot 3</a></li>
+				<li>
+					<a href="docs/JSON-callout.zip">Code</a>
+				</li>
+			</ul>
 			<li><a href="docs/Support_Site_Requirements.docx">Support Site Requirements</a></li>
 			<li><a href="docs/Support_Site_Screens.docx">Support Site Screens</a></li>
 			<li><a href="/switchbuilder">jQuery/AJAX Demo - Screencast</a></li>
