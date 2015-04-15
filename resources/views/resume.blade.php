9978124901b7e067fb143a96f36756c9edea6f0e
Jim Baldwin <jim@hvaldwin.net>
2015-04-15 09:35:24 -0700

    remove address from resume

--- resources/views/resume.blade.php
+++ resources/views/resume.blade.php
@@ -4,11 +4,9 @@
 	<div class="container resume">
 <p class="western" align="center" style="margin-bottom: 0in"><font size="2" style="font-size: 11pt"><b>Jim
 Baldwin</b></font></p>
-<p class="western" align="center" style="margin-bottom: 0in"><font size="2" style="font-size: 11pt">3364
-SE 8</font><sup><font size="2" style="font-size: 11pt">th</font></sup><font size="2" style="font-size: 11pt">
-Ave, Portland, OR 97202</font></p>
 <p class="western" align="center" style="margin-bottom: 0in"><font size="2" style="font-size: 11pt">503-231-3735
-home, 503-522-0466 cell</font></p>
+home</font></p>
+<p class="western" align="center" style="margin-bottom: 0in"><font size="2" style="font-size: 11pt">503-522-0466 cell</font></p>
 <p class="western" align="center" style="margin-bottom: 0in"><font size="2" style="font-size: 11pt">jim@jimbaldwin.net</font></p>
 <p class="western" style="margin-bottom: 0in"><br>
 </p>
