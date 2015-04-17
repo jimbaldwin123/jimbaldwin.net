2c782e7b29431b632eaaa87f9115f837e358dbcb
Jim Baldwin <jim@hvaldwin.net>
2015-04-17 13:52:44 -0700

    fonts

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -7,10 +7,10 @@
 	<title>PHP Developer, Portland, Or | LAMP Stack Web Developer | Jim Baldwin</title>
 	<meta name="description" content="LAMP Stack Web Developer in Portland, OR. Web Sites, MVC, REST" >
 	<link href="{{ asset('/css/app.css') }}" rel="stylesheet">
-
-	<!-- Fonts -->
-	<link href='//fonts.googleapis.com/css?family=Roboto:400,300' rel='stylesheet' type='text/css'>
-
+	<link href='http://fonts.googleapis.com/css?family=Lato' rel='stylesheet' type='text/css'>
+	<style type="text/css">
+		body{ font-family:jeannie_font;}
+	</style>
 	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
 	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
 	<!--[if lt IE 9]>
