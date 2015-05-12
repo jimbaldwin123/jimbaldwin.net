6296d156e7f71c458f1f411cb876ca25cf067cde
Jim Baldwin <jim@hvaldwin.net>
2015-05-12 08:42:49 -0700

    add fb buttons 3

--- resources/views/work.blade.php
+++ resources/views/work.blade.php
@@ -60,30 +60,34 @@
 			<li><a href="docs/Support_Site_Screens.docx">Support Site Screens</a> &ndash; Some jQuery/AJAX functionality</li>
 			
 		</ul>
-		<div>
-			<script>
-				window.fbAsyncInit = function() {
-				  FB.init({
-					appId      : '883071591739100',
-					xfbml      : true,
-					version    : 'v2.3'
-				  });
-				};
-			  
-				(function(d, s, id){
-				   var js, fjs = d.getElementsByTagName(s)[0];
-				   if (d.getElementById(id)) {return;}
-				   js = d.createElement(s); js.id = id;
-				   js.src = "//connect.facebook.net/en_US/sdk.js";
-				   fjs.parentNode.insertBefore(js, fjs);
-				 }(document, 'script', 'facebook-jssdk'));
-			</script>
-			<div
-			  class="fb-like"
-			  data-share="true"
-			  data-width="450"
-			  data-show-faces="true">
-			</div>
+		<ul class="section">
+			<li>
+				<script>
+					window.fbAsyncInit = function() {
+					  FB.init({
+						appId      : '883071591739100',
+						xfbml      : true,
+						version    : 'v2.3'
+					  });
+					};
+				  
+					(function(d, s, id){
+					   var js, fjs = d.getElementsByTagName(s)[0];
+					   if (d.getElementById(id)) {return;}
+					   js = d.createElement(s); js.id = id;
+					   js.src = "//connect.facebook.net/en_US/sdk.js";
+					   fjs.parentNode.insertBefore(js, fjs);
+					 }(document, 'script', 'facebook-jssdk'));
+				</script>
+				<div
+				  class="fb-like"
+				  data-share="true"
+				  data-width="450"
+				  data-show-faces="true">
+				</div>				
+			</li>
+		</ul>
+
 		</div>
 	</div>
 
