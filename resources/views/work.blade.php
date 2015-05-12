926c388328d01b9ad153340fe3cebc66a3ddcaee
Jim Baldwin <jim@hvaldwin.net>
2015-05-12 08:38:44 -0700

    add fb buttons

--- resources/views/work.blade.php
+++ resources/views/work.blade.php
@@ -61,4 +61,29 @@
 			
 		</ul>
 	</div>
+	<div>
+		<script>
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '883071591739100',
+      xfbml      : true,
+      version    : 'v2.3'
+    });
+  };
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "//connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+</script>
+<div
+  class="fb-like"
+  data-share="true"
+  data-width="450"
+  data-show-faces="true">
+</div>
+	</div>
 @endsection
